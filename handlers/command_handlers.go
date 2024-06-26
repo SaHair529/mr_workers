3a487a305d2c5f9a9deb5d42c22b800be6452881
SaHair529 <sa.hairulaev@gmail.com>
2024-06-26 18:45:15 +0300

    Добавил хранение ответов в json файле

--- handlers/command_handlers.go
+++ handlers/command_handlers.go
@@ -1,20 +1,40 @@
 package handlers
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+import (
+    "encoding/json"
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+    "io/ioutil"
+)
 
 type CommandHandler struct {
 	bot *tgbotapi.BotAPI
+	messages CommandHandlerMessages
+}
+
+type CommandHandlerMessages struct {
+	Registration string `json:"registration"`
 }
 
 func NewCommandHandler(bot *tgbotapi.BotAPI) *CommandHandler {
+	messagesJson, err := ioutil.ReadFile("messages.json")
+	onFail("Failed to read file %v", err)
+	var messages CommandHandlerMessages
+	err = json.Unmarshal(messagesJson, &messages)
+	onFail("Failed to unmarshal json %v", err)
+
 	return &CommandHandler{
 		bot: bot,
+		messages: messages,
 	}
 }
 
 func (h *CommandHandler) HandleCommand(message *tgbotapi.Message) {
-	responseMsg := tgbotapi.NewMessage(message.Chat.ID, "Command "+message.Command())
+    switch message.Command() {
+    case "registration":
+		responseMsg := tgbotapi.NewMessage(message.Chat.ID, h.messages.Registration)
+		_, err := h.bot.Send(responseMsg)
+		onFail("Failed to send message %v", err)
+    default:
 
-	_, err := h.bot.Send(responseMsg)
-	onFail("Failed to send message %v", err)
+    }
 }
